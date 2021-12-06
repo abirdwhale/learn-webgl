c4c759aad6447361e081087fbea88e24427164e4
Masa, abirdwhale Kakinoki Masato, PhD / now London <m@kakinokimasato.com>
2021-12-06 15:40:23 +0000

    "Getting started with WebGL" done

---
+++ webgl-tutorial/Getting-started-with-WebGL/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//
+// start here
+//
+function main() {
+  const canvas = document.querySelector("#glCanvas");
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl2");
+
+  // Only continue if WebGL2 is available and working
+  if (gl === null) {
+    alert("Unable to initialize WebGL2. Your browser or machine may not support it.");
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+window.onload = main;
